74f5fc95ed19d11e7bc00a1e54729957523c2c5c
JShafer92 <ptwraith1@gmail.com>
2018-06-07 16:57:51 -0700

    frame code and psuedo code created

---
+++ assets/javascript/triviaGame.js
@@ -0,0 +1,26 @@
+// var trivia questions
+var questions = [{
+    question:'Who wrote Romio and Juliet?',
+    choices: ['William Shakespear','William Shatner', 'Will.I.Am','William Clinton']}]
+
+// var trivia answer arrays
+
+// 
+
+//countdown timer
+
+//at end of timer display correct and incorrect answers
+
+
+
+//You'll create a trivia form with multiple choice or true/false options (your choice).
+
+// The player will have a limited amount of time to finish the quiz. 
+
+// The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+// Don't let the player pick more than one answer per question.
+
+// include a countdown timer.
+
+//reset button
